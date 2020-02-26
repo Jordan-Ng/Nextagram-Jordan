1255d2d035f71567ebf47f3eaf808cdbdde37982
Jordan <jordanNg@Jordans-MacBook-Pro.local>
2020-02-26 13:32:23 +0800

    setup new.html

--- models/user.py
+++ models/user.py
@@ -3,4 +3,6 @@ import peewee as pw
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    name = pw.CharField(null=False, unique=False)
+    email = pw.CharField(null=False, unique=True)
+    password = pw.CharField(null=False, unique=False)
