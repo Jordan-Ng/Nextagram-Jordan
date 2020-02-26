9137f84047597f194d7698c3d1a8880251d8593e
Jordan <jordanNg@Jordans-MacBook-Pro.local>
2020-02-26 17:07:59 +0800

    signup page now with username, email, password validation

--- models/base_model.py
+++ models/base_model.py
@@ -20,13 +20,25 @@ class BaseModel(pw.Model):
 
     def validate(self):
         from models.user import User
+        import re
         duplicate_username = User.get_or_none(User.name == self.name)
         duplicate_email = User.get_or_none(User.email == self.email)
+        string_check = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
         if duplicate_username:
             self.errors.append('username is taken!')
         if duplicate_email:
             self.errors.append(
                 'email address is associated with another account!')
+        if "@" not in self.email:
+            self.errors.append('please enter a valid email')
+        if(string_check.search(self.password) == None):
+            self.errors.append(
+                'password must contain at least one special character')
+        if len(self.password) <= 6:
+            self.errors.append('password has to be more than 6 characters')
+        if self.password.isupper() or self.password.islower():
+            self.errors.append(
+                'password must be a combination of upper and lower case characters')
         # print(
         #     f"Warning validation method not implemented for {str(type(self))}")
         # return True
